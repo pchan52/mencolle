7e026d9243f067a4bdccbb4405d56d4e36ebcebb
池田光 <Hikaru@pchan.local>
2017-05-24 13:38:05 +0900

    Create MstCharacter.cs to set value from csv

---
+++ Assets/MenColle/Scripts/MstCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class MstCharacter : MonoBehaviour {
+
+	[SerializeField]
+	private int
+	id,
+	rarity,
+	maxLebel,
+	growthType,
+	lowerEnergy,
+	upperEnergy,
+	initialCost;
+
+	[SerializeField]
+	private string 
+	name,
+	imageId,
+	flavorText;
+
+	//dataを受け取って値を変数に格納
+	public void SetFromCSV(string[] data){
+		id = int.Parse (data [0]);
+		name = data [1];
+		imageId = data [2];
+		flavorText = data [3];
+		rarity = int.Parse (data [4]);
+		maxLebel = int.Parse (data [5]);
+		growthType = int.Parse (data [6]);
+		lowerEnergy = int.Parse (data [7]);
+		upperEnergy = int.Parse (data [8]);
+		initialCost = int.Parse (data [9]);
+	}
+}
