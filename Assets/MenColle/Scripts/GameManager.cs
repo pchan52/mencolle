2ce97c83237b6523169ad4fe1f67cd1dabe50d6d
池田光 <Hikaru@pchan.local>
2017-05-26 00:45:28 +0900

    Create GameManager.cs to load data by calling MasterDataManager

---
+++ Assets/MenColle/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager
+	: SingletonMonoBehaviour<GameManager>
+{
+	private void Start()
+	{
+		MasterDataManager.instance.LoadData ();
+	}
+
+	public static void Log (object log)
+	{
+		if (Debug.isDebugBuild)
+		{
+			Debug.Log(log);
+		}
+	}
+}
