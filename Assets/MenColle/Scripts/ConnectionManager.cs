fbb3700ab8fae4f0e9c5c4c66b2331898e9ce99d
池田光 <Hikaru@pchan.local>
2017-05-26 00:43:54 +0900

    Create ConnectionManager.cs and Attach to control connection

---
+++ Assets/MenColle/Scripts/ConnectionManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConnectionManager : SingletonMonoBehaviour<ConnectionManager> {
+
+	public void ConnectionAPI (
+		string url,
+		UnityAction<string> onFinish = null,
+		UnityAction<string> errorFinish = null
+	) {
+		StartCoroutine(ConnectionAPICoroutine(url, onFinish, errorFinish));
+	}
+
+	private IEnumerator ConnectionAPICoroutine (
+		string url,
+		UnityAction<string> onFinish = null,
+		UnityAction<string> errorFinish = null
+	) {
+		WWW www = new WWW(url);
+//		GameManager.Log("通信開始 : " + www.url);
+		yield return www;
+		if (www.error == null) {
+//			GameManager.Log( "url:"+ www.url + "\nSuccess : " + www.text );
+			if (onFinish != null) {
+				onFinish( www.text );
+			}
+		}
+		else {
+//			GameManager.Log("url:"+ www.url + "\nFaild : " + www.error);
+			if (errorFinish != null) {
+				errorFinish( www.error );
+			}
+		}
+	}
+
+}
