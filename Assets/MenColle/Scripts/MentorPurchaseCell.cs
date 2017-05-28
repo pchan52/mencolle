f8a08c43c2d2b3af185db3ae31fb0a1c89f5dd55
池田光 <Hikaru@pchan.local>
2017-05-28 23:46:28 +0900

    Import face image

---
+++ Assets/MenColle/Scripts/MentorPurchaseCell.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MentorPurchaseCell : MonoBehaviour
+{
+	[SerializeField] private Image iconImage;
+	[SerializeField] private Text
+	nameLabel,
+	rarityLabel,
+	flavorTextLabel,
+	productivityLabel,
+	costLabel;
+
+	[SerializeField] private Button purchaseButton;
+
+	private bool isSold = false;
+	private MstCharacter characterData;
+
+	public void SetValue(MstCharacter data)
+	{
+		iconImage.sprite = Resources.Load<Sprite>("Face/" + data.ImageId);
+		characterData = data;
+		nameLabel.text = data.Name;
+		rarityLabel.text = "";
+		for (int i = 0; i < data.Rarity; i++) { rarityLabel.text += "★"; }
+		flavorTextLabel.text = data.FlavorText;
+		productivityLabel.text = "生産性(lv.1) : " + data.LowerEnergy;
+		costLabel.text = string.Format("¥{0:#,0}", data.InitialCost);
+	}
+}
