ac212365ac0bca106a59d54bd98b2e09ab729b66
池田光 <Hikaru@pchan.local>
2017-05-30 16:49:13 +0900

    Create User.cs to manage User data

---
+++ Assets/MenColle/Scripts/User/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class User
+{
+
+	[SerializeField] private int _money;
+	[SerializeField] private List<Character> _characters;
+
+	public int Money {
+		get { return _money; }
+	}
+
+	public List<Character> Characters
+	{
+		get { return _characters ?? (_characters = new List<Character>()); }
+	}
+	
+	public Character NewCharacter(MstCharacter data)
+	{
+		var uniqueId = (Characters.Count == 0) ? 1 : _characters[_characters.Count - 1].UniqueID + 1;
+		var chara = new Character(uniqueId, data);
+		_characters.Add(chara);
+		_money -= data.InitialCost;
+		return chara;
+	}
+}
