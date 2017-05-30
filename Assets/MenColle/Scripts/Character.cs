a043f2c85c56991f2e995f1ac73e84f83c96a1cf
池田光 <Hikaru@pchan.local>
2017-05-30 15:51:21 +0900

    Create Character.cs to management characters

--- Assets/MenColle/Scripts/Character.cs
+++ Assets/MenColle/Scripts/Character.cs
@@ -3,16 +3,33 @@
 [System.SerializableAttribute]
 public class Character {
 
-	[SerializeField] private int _uid, _masterId, _level;
+	[SerializeField] private int _uId, _masterId, _level;
 
-	public int UniqueId { get {return _uid;} }
-	public int MasterId { get {return _masterId;} }
+	public int UniqueID { get {return _uId;} }
+	public int MasterID { get {return _masterId;} }
 	public int Level { get {return _level;} }
 
-	public Character(int uniqueId, MstCharacter chr)
+	public MstCharacter Master
 	{
-		_uid = uniqueId;
+		get { return MasterDataManager.instance.GetCharacterById(_masterId); }
+	}
+
+	public Character(int uniqueID, MstCharacter chr)
+	{
+		_uId = uniqueID;
 		_masterId = chr.ID;
 		_level = 1;
+	}
+
+	public int Power
+	{
+		get{
+			return 1;
+		}
+	}
+
+	public bool IsLevelMax
+	{
+		get { return (_level >= Master.MaxLevel) ? true : false; }
 	}
 }
