87612b97e9ab42498f8e89d1dcfb90c255cb1b2b
池田光 <Hikaru@pchan.local>
2017-05-30 13:13:12 +0900

    Create Character.cs to have character information

---
+++ Assets/MenColle/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[System.SerializableAttribute]
+public class Character {
+
+	[SerializeField] private int _uid, _masterId, _level;
+
+	public int UniqueId { get {return _uid;} }
+	public int MasterId { get {return _masterId;} }
+	public int Level { get {return _level;} }
+
+	public Character(int uniqueId, MstCharacter chr)
+	{
+		_uid = uniqueId;
+		_masterId = chr.ID;
+		_level = 1;
+	}
+}
