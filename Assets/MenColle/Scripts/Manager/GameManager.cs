0608196833fef1077406487404f650d2d5327cfb
池田光 <Hikaru@pchan.local>
2017-06-04 23:50:24 +0900

    Edit GameManager.cs to save as json

--- Assets/MenColle/Scripts/Manager/GameManager.cs
+++ Assets/MenColle/Scripts/Manager/GameManager.cs
@@ -6,10 +6,20 @@ public class GameManager
 {
 	[SerializeField] private User userData = new User();
 	public User User { get { return userData; } }
+	private const string SaveKey = "SaveData";
 	
 	private void Start()
 	{
+		if (PlayerPrefs.HasKey(SaveKey))
+		{
+			userData = JsonUtility.FromJson<User>(PlayerPrefs.GetString(SaveKey));
+		}
 		MasterDataManager.instance.LoadData ();
+	}
+
+	public void Save()
+	{
+		PlayerPrefs.SetString(SaveKey, JsonUtility.ToJson(userData));
 	}
 
 	public static void Log (object log)
